5fe62d14a455b2582565876a595087c1b2687a75
sabrinavohra <sabrina.vohra@menloschool.org>
2025-04-10 09:52:29 -0700

    Edit distance and main method.

--- src/main/java/Movie.java
+++ src/main/java/Movie.java
@@ -3,23 +3,52 @@ import info.movito.themoviedbapi.model.movies.Images;
 
 public class Movie {
     private String name;
-    private double length;
+    private int length;
     private int budget;
-    private Genre genre;
+    private String genre;
     private String overview;
     private Images poster;
 
-    public Movie(String name, double length, int budget, Genre genre, String overview, Images poster) {
+    public Movie(String name, int length, int budget, Genre genre, String overview, Images poster) {
         this.name = name;
         this.length = length;
         this.budget = budget;
-        this.genre = genre;
+        this.genre = genre.getName();
         this.overview = overview;
         this.poster = poster;
+    }
+
+    public Movie(int length, int budget, String genre) {
+        this.length = length;
+        this.budget = budget;
+        this.genre = genre;
     }
 
     public String toString() {
         return "Title: " + name + "\nLength: " + length + "\nBudget: " + budget + "\nGenre: " + genre + "\nOverview:" + overview + "\nPoster: " + poster + "\n\n\n";
     }
 
+    public String getName() {
+        return this.name;
+    }
+
+    public int getLength() {
+        return this.length;
+    }
+
+    public int getBudget() {
+        return this.budget;
+    }
+
+    public String getGenre() {
+        return this.genre;
+    }
+
+    public String getOverview() {
+        return this.overview;
+    }
+
+    public Images getPoster()  {
+        return this.poster;
+    }
 }
--- src/main/java/tester.java
+++ src/main/java/tester.java
@@ -5,11 +5,52 @@ import info.movito.themoviedbapi.tools.TmdbException;
 import info.movito.themoviedbapi.tools.appendtoresponse.MovieAppendToResponse;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class tester {
     private static ArrayList<Movie> movieList = new ArrayList<Movie>();
-    public static void main(String[] args) throws TmdbException {
 
+    public static void main(String[] args) throws TmdbException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("How long would you like your movie to be?");
+        int len = input.nextInt();
+        System.out.println("What should the budget be?");
+        int budg = input.nextInt();
+        System.out.println("What genre?");
+        input.nextLine();
+        String gen = input.nextLine();
+        Movie user = new Movie(len, budg, gen);
+        Movie theClosest = closest(user);
+        System.out.println(theClosest);
+    }
+
+    public static Movie closest (Movie a) {
+        Movie current = null;
+        int highest = 0;
+        for(int i = 0; i < movieList.size(); i++) {
+            int c = editDistance(a, movieList.get(i));
+            if(c > highest) {
+                current = movieList.get(i);
+            }
+        }
+        return current;
+    }
+
+    public static int editDistance(Movie a, Movie b) {
+        int total = 0;
+        if(Math.abs(a.getLength() - b.getLength()) <= 15) {
+            total++;
+        }
+        if(Math.abs(a.getBudget() - b.getBudget()) <= 100) {
+            total++;
+        }
+        if(a.getGenre().equals(b.getGenre())) {
+            total++;
+        }
+        return total;
+    }
+
+    public void load () throws TmdbException {
         TmdbApi tmdbApi = new TmdbApi("eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMDkwNjk5ZGE1ODg4ZGI5MjE1ZGNmMGNhMjgwZDZmYiIsIm5iZiI6MTc0MzE5MjczMi42ODUsInN1YiI6IjY3ZTcwMjljMDkyNTI4NjJlYTc2N2U4NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Q08Y593tZg-jbJ1WUG5Q5QV4Wc_NeCiZ4nM9x0JHb3A");
         TmdbMovies tmdbMovies = tmdbApi.getMovies();
         MovieDb movie = tmdbMovies.getDetails(5353, "en-US");
@@ -18,9 +59,6 @@ public class tester {
             MovieDb current = tmdbMovies.getDetails(i, "en-US", MovieAppendToResponse.IMAGES);
             Movie movie2 = new Movie(current.getTitle(), current.getRuntime(), current.getBudget(), current.getGenres().getFirst(), current.getOverview(), current.getImages());
             movieList.add(movie2);
-        }
-        for(int i = 0; i < movieList.size(); i++) {
-            System.out.println(movieList.get(i));
         }
     }
 }
